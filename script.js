64dec8364fec75031800ee999b6074364fbb3777
abhiavi7 <118717274+abhiavi7@users.noreply.github.com>
2023-12-30 20:44:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,34 @@
-class Animal {
-  constructor(species) {
-    this._species = species;
-  }
+// Test for Cat
+it("Cat species", () => {
+  cy.visit(baseUrl + "/main.html");
+  cy.window().then((win) => {
+    const Cat = win.window.Cat; // Assuming Cat is defined in the window object
+    cy.stub(win.console, "log").as("consoleLog");
 
-  get species() {
-    return this._species;
-  }
+    const species = "Siamese";
+    const myCat = new Cat(species);
+    myCat.makeSound();
 
-  makeSound() {
-    console.log(`The ${this.species} makes a sound.`);
-  }
-}
+    cy.get("@consoleLog").should("be.calledWith", `The ${species} makes a sound`);
+    myCat.purr();
+    cy.get("@consoleLog").should("be.calledWith", `purr`);
+  });
+});
 
-class Cat extends Animal {
-  purr() {
-    console.log("purr");
-  }
-}
+// Test for Dog
+it("Dog species", () => {
+  cy.visit(baseUrl + "/main.html");
+  cy.window().then((win) => {
+    const Dog = win.window.Dog; // Assuming Dog is defined in the window object
+    cy.stub(win.console, "log").as("consoleLog");
 
-class Dog extends Animal {
-  bark() {
-    console.log("woof");
-  }
-}
+    const species = "Golden Retriever";
+    const myDog = new Dog(species);
+    myDog.makeSound();
 
-// Example usage:
-const cat = new Cat("Cat");
-console.log(cat.species);  // Output: Cat
-cat.makeSound();  // Output: The Cat makes a sound.
-cat.purr();  // Output: purr
+    cy.get("@consoleLog").should("be.calledWith", `The ${species} makes a sound`);
+    myDog.bark();
+    cy.get("@consoleLog").should("be.calledWith", `woof`);
+  });
+});
 
-const dog = new Dog("Dog");
-console.log(dog.species);  // Output: Dog
-dog.makeSound();  // Output: The Dog makes a sound.
-dog.bark();  // Output: woof
